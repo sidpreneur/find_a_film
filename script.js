f062a8d21b5bf44c7f5f573d02550da3d77376eb
Siddhant Singh <68225026+sidpreneur@users.noreply.github.com>
2024-12-24 11:38:28 +0530

    Add files via upload
    adding the necessary files

---
+++ script.js
@@ -0,0 +1,50 @@
+const myButton = document.querySelector('#myButton');
+const apiKey = 'bd53b199';
+const type = 'movie';
+const result=document.getElementById('result');
+const page = 1;
+
+myButton.addEventListener('click', () => {
+    result.innerHTML= '';
+  const query = document.querySelector('#moviee').value;
+  const year = document.querySelector('#yr').value;
+
+
+  const url = `http://www.omdbapi.com/?s=${encodeURIComponent(query)}&type=${type}&y=${year}&page=${page}&apikey=${apiKey}`;
+
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Status:${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.Response === "True") {
+        console.log("Search Results:", data.Search);
+        data.Search.forEach((movie) => {
+          const movieElement = document.createElement('div');
+          movieElement.classList.add('movie');
+          movieElement.innerHTML = `
+            <h3>${movie.Title} (${movie.Year})</h3>
+            <img src="${movie.Poster}" alt="${movie.Title}" style="width: 100px;">
+            <p>IMDB ID: ${movie.imdbID}</p>
+          `;
+          result.appendChild(movieElement);
+        });
+      } else {
+        console.error("Error:", data.Error);
+        const movieElement = document.createElement('div');
+          movieElement.classList.add('movie');
+          movieElement.innerHTML = `
+            <h3>Movie Not found!</h3>
+          `;
+          result.appendChild(movieElement);
+
+      }
+    })
+    .catch(error => {
+      console.error("Fetch Error:", error);
+    });
+});
